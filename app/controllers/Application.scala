3fb30bda19aec607b9e949d7501008fdcfb661bf
Aoino <aoiro.aoino@gmail.com>
2014-08-06 14:21:58 +0900

    initial commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,26 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import scala.concurrent._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+object Application extends Controller {
+  private val logger = Logger(getClass())
+
+  def index = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
+
+  def nonblocking = Action.async {
+    val loooongTimeTaskFuture = Future {
+      List.range(0,10).foreach { i => 
+        Thread.sleep(1000)
+        logger.info("Count: " +  i)
+      }
+    }
+    val dummyFuture = Future { "Dummy Future" }
+
+    dummyFuture.map(Ok(_))
+  }
+}
